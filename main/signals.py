1ab44bf07c2b4c371aaf42ed05ef6e53b5649621
Carlos Perez <charlsagente@gmail.com>
2019-07-21 10:56:25 -0600

    adding signals for DB

---
+++ main/signals.py
@@ -0,0 +1,42 @@
+from io import BytesIO
+import logging
+from PIL import Image
+from django.core.files.base import ContentFile
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+from .models import ProductImage
+
+THUMBNAIL_SIZE = (300, 300)
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(pre_save, sender=ProductImage)
+def generate_thumbnail(sender, instance, **kwargs):
+    """
+
+    :param sender:
+    :param instance:
+    :type instance ProductImage
+    :param kwargs:
+    :return:
+    """
+    logger.info(
+        "Generating thumbnail for product %d",
+        instance.product.id,
+    )
+    image = Image.open(instance.image)
+    image = image.convert("RGB")
+    image.thumbnail(THUMBNAIL_SIZE, Image.ANTIALIAS)
+
+    temp_thumb = BytesIO()
+    image.save(temp_thumb, "JPEG")
+    temp_thumb.seek(0)
+
+    instance.thumbnail.save(
+        instance.image.name,
+        ContentFile(temp_thumb.read()),
+        save=False,
+    )
+    temp_thumb.close()
+
