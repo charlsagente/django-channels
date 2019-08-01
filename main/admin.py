48c96d3e7fb98c5afa553e822ddd481e4b2c8694
charlsagente <charlsagente@gmail.com>
2019-07-31 21:40:45 -0700

    adding signup form

--- main/admin.py
+++ main/admin.py
@@ -1,14 +1,55 @@
 from django.contrib import admin
 from . import models
 from django.utils.html import format_html
-from django.contrib.auth.
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
 
 # Register your models here.
 
 
+@admin.register(models.User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal info",
+            {"fields": ("first_name", "last_name")},
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "is_staff",
+    )
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'in_stock', 'price')
-    list_filter = ('active', 'in_stock', 'date_updated')
+    list_display = ('name', 'slug', 'in_stock', 'price',)
+    list_filter = ('active', 'in_stock', 'date_updated',)
     list_editable = ('in_stock',)
     search_fields = ('name',)
     prepopulated_fields = {"slug": ('name',)}
@@ -18,18 +59,18 @@ admin.site.register(models.Product, ProductAdmin)
 
 
 class ProductTagAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug')
+    list_display = ('name', 'slug',)
     list_filter = ('active',)
     search_fields = ('name',)
     prepopulated_fields = {"slug": ("name",)}
-    #autocomplete_fields = ('tags',)
+    # autocomplete_fields = ('tags',)
 
 
 admin.site.register(models.ProductTag, ProductTagAdmin)
 
 
 class ProductImageAdmin(admin.ModelAdmin):
-    list_display = ('thumbnail_tag', 'product_name')
+    list_display = ('thumbnail_tag', 'product_name',)
     readonly_fields = ('thumbnail',)
     search_fields = ('product__name',)
 
--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,10 @@
 from django import forms
 from django.core.mail import send_mail
+from django.contrib.auth.forms import (
+    UserCreationForm as DjangoUserCreationForm
+)
+from django.contrib.auth.forms import UsernameField
+from . import models
 import logging
 
 logger = logging.getLogger(__name__)
@@ -21,4 +26,25 @@ class ContactForm(forms.Form):
             "site@booktime.domain",
             ["customerservice@booktime.domain"],
             fail_silently=False,
+        )
+
+
+class UserCreationForm(DjangoUserCreationForm):
+    class Meta(DjangoUserCreationForm.Meta):
+        model = models.User
+        fields = ("email",)
+        field_classes = {"email": UsernameField}
+
+    def send_mail(self):
+        logger.info(
+            "Sending signup email for email=%s",
+            self.cleaned_data["email"],
+        )
+        message = "Welcome{}".format(self.cleaned_data["email"])
+        send_mail(
+            "Welcome to BookTime",
+            message,
+            "site@booktime.domain",
+            [self.cleaned_data["email"]],
+            fail_silently=True,
         )
