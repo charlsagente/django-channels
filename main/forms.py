dddd438e56c77208ecf4aff49555eff5349793cd
charlsagente <charlsagente@gmail.com>
2019-08-17 22:24:05 -0700

    adding complete busket logic

--- main/forms.py
+++ main/forms.py
@@ -93,3 +93,17 @@ BasketLineFormSet = inlineformset_factory(
     widgets={"quantity": widgets.PlusMinusNumberInput()},
 )
 
+
+class AddressSelectionForm(forms.Form):
+    billing_address = forms.ModelChoiceField(
+        queryset=None
+    )
+    shipping_address = forms.ModelChoiceField(
+        queryset=None
+    )
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        queryset = models.Address.objects.filter(user=user)
+        self.fields['billing_address'].queryset = queryset
+        self.fields['shipping_address'].queryset = queryset
--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,16 @@ from main import views, models, forms
 
 urlpatterns = [
     path(
+        "order/done/",
+        TemplateView.as_view(template_name="order_done.html"),
+        name="checkout_done",
+    ),
+    path(
+        "order/address_select/",
+        views.AddressSelectionView.as_view(),
+        name="address_select",
+    ),
+    path(
         "address/",
         views.AddressListView.as_view(),
         name="address_list",
