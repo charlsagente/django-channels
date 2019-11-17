162ae57f9701a06a500f9770e66f31087e573068
charlsagente <charlsagente@gmail.com>
2019-11-16 22:59:24 -0800

    Added async tests for Websockets sessions

--- main/tests/test_consumers.py
+++ main/tests/test_consumers.py
@@ -2,9 +2,13 @@ import asyncio
 from django.contrib.auth.models import Group
 from django.test import TestCase
 from channels.db import database_sync_to_async
-from channels.testing import WebsocketCommunicator
+from channels.testing import (
+    WebsocketCommunicator,
+    HttpCommunicator,
+)
 from main import consumers
 from main import factories
+
 
 class TestConsumers(TestCase):
     def test_chat_between_two_users_works(self):
@@ -42,12 +46,27 @@ class TestConsumers(TestCase):
             }
             connected, _ = await communicator.connect()
             self.assertTrue(connected)
-            await communicator.send_json_to({
-                "type": "message",
-                "message": "hello customer service",
-            }
+
+            cs_communicator = WebsocketCommunicator(
+                consumers.ChatConsumer,
+                "/ws/customer-service/%d/" % order.id,
             )
+            cs_communicator.scope['user'] = cs_user
+            cs_communicator.scope['url_route'] = {
+                "kwargs": {'order_id': order.id}
+            }
+            connected, _ = await cs_communicator.connect()
+            self.assertTrue(connected)
+
+            await communicator.send_json_to(
+                {
+                    "type": "message",
+                    "message": "hello customer service",
+                }
+            )
+
             await asyncio.sleep(1)
+
             await cs_communicator.send_json_to(
                 {"type": "message", "message": "hello user"}
             )
@@ -57,6 +76,57 @@ class TestConsumers(TestCase):
             )
             self.assertEquals(
                 await communicator.receive_json_from(),
-            {"type": "chat_join", "username": "Adam Ford"},
+                {"type": "chat_join", "username": "Adam Ford"},
+            )
+            self.assertEquals(
+                await communicator.receive_json_from(),
+                {
+                    "type": "chat_message",
+                    "username": "John Smith",
+                    "message": "hello customer service",
+                },
+            )
+            self.assertEquals(
+                await communicator.receive_json_from(),
+                {
+                    'type': 'chat_message',
+                    'username': 'Adam Ford',
+                    'message': 'hello user',
+                },
             )
 
+            await communicator.disconnect()
+            await cs_communicator.disconnect()
+
+            order.refresh_from_db()
+            self.assertEquals(order.last_spoken_to, cs_user)
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(test_body())
+
+    def test_chat_blocks_unauthorized_users(self):
+        def init_db():
+            user = factories.UserFactory(
+                email="john@bestemails.com",
+                first_name="John",
+                last_name="Smith",
+            )
+            order = factories.OrderFactory()
+
+            return user, order
+
+        async def test_body():
+            user, order = await database_sync_to_async(init_db)()
+            communicator = WebsocketCommunicator(
+                consumers.ChatConsumer,
+                "/ws/customer-service/%d/" % order.id
+            )
+            communicator.scope['user'] = user
+            communicator.scope['url_route'] = {
+                "kwargs": {'order_id': order.id}
+            }
+            connected, _ = await communicator.connect()
+            self.assertFalse(connected)
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(test_body())
